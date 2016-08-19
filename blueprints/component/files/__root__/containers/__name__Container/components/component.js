f0c0022edc53611b2c12b08d3930824b326d215d
jingxin.sjx <jingxin.sjx@alibaba-inc.com>
2016-08-19 15:19:25 +0800

    增加component脚手架

---
+++ blueprints/component/files/__root__/containers/__name__Container/components/component.js
@@ -0,0 +1,18 @@
+import React, {Component, PropTypes} from 'react';
+
+export default class <%= pascalEntityName %>Component extends Component {
+  constructor(props) {
+  super(props);
+}
+
+  componentDidMount() {
+  const {actions} = this.props
+}
+
+  render() {
+  return (
+  <div>
+  </div>
+  );
+  }
+  }
